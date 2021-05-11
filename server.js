c0922aabb2e1f824c0d0eb519f84750de4b14ed4
Diego Aspinwall <diespinwall@gmail.com>
2021-05-11 18:25:57 -0400

    Mr. X

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello, mundo!');
 });
 
+app.get('/x', function (req, res) {
+  res.send('Hello, mr. X!');
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('myapp listening on port ' + port);

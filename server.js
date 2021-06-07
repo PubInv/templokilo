ef9fc8d0e0f43117be25b78c46918493eaece52c
Diego Aspinwall <diespinwall@gmail.com>
2021-06-07 18:13:06 -0400

    cleanup server

--- server.js
+++ server.js
@@ -4,17 +4,10 @@ const bodyParser = require("body-parser");
 const app = express();
 
 
-// Firebase App (the core Firebase SDK) is always required and
-// must be listed before other Firebase SDKs
 var firebase = require("firebase/app");
 
-// Add the Firebase products that you want to use
 require("firebase/auth");
 require('firebase/database');
-
-console.dir(firebase);
-console.dir("XXXX")
-console.dir(firebase.apps);
 
 var config = {
     apiKey: process.env.apiKey,
@@ -28,10 +21,8 @@ var config = {
 firebase.initializeApp(config);
 
 firebase.auth().signInAnonymously().catch(function(error) {
-    // Handle Errors here.
     var errorCode = error.code;
     var errorMessage = error.message;
-    // ...
     console.log(errorCode);
     console.log(errorMessage);
 });
@@ -42,7 +33,6 @@ app.use(express.static(__dirname));
 
 app.use(bodyParser.urlencoded({ extended:false }));
 app.use(bodyParser.json());
-//app.use(express.bodyParser());
 
 function returnFirebaseSnapshot(req, ref, res) {
     var appName = req.query.appName;
@@ -84,36 +74,24 @@ app.post('/writeTag', function (req, res){
    });*/
 
 app.get('/writeTag', function (req, res) {
-    //console.log(req.query.appname);
-    //console.log(req.query.tagId);
-    //console.log(req.query.taginfo);
     var obj = req.query.taginfo;
     obj["latitude"] = parseFloat(obj.latitude);
-    obj["longitude"] = parseFloat(obj.longitude);
-    //console.log(obj);
-    
+    obj["longitude"] = parseFloat(obj.longitude);    
     firebase.database().ref('/apps/' + req.query.appname + "/tags/" + req.query.tagId).set(obj,
                                                  function(error) {
                                                      if (error) {
                                                          console.log("ERROR:",error);
-                                                     } else {
-                                                         console.log("SUCCESS");
                                                      }
                                                  });
 });
 
 app.get('/actuallyCreate', function (req, res) {
-    console.log(req.query.appname);
-    //console.log(req.query.taginfo);
-    console.log(req.query.obj);
     var config = req.query.obj;
     for (const property in config) {
 	if (config[property] == "false") {config[property] = false;}
 	if (config[property] == "true") {config[property] = true;}
     }
     config.tags = {};
-    //config.push(tags : {});
-    console.log(config);
     firebase.database().ref('apps/' + req.query.appname).set(config,
 						   function(error) {
 						       if (error) {
